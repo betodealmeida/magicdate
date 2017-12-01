14bca3b9eef972aaa2562b7dc4f76936870e2231
Beto Dealmeida <roberto@dealmeida.net>
2017-11-30 20:18:06 -0800

    Version 0.2, use as command line utility.

--- magicdate.py
+++ magicdate.py
@@ -1,15 +1,17 @@
+#!/usr/bin/env python
+
+import calendar
+from copy import copy
+import datetime
+from optparse import Option, OptionParser, OptionValueError
 import os
 import re
-import datetime
-import calendar
-from optparse import Option, OptionValueError
-from copy import copy
 
 
 def check_magicdate(option, opt, value):
     try:
         return magicdate(value)
-    except:
+    except Exception:
         raise OptionValueError(
             "option %s: invalid date value: %r" % (opt, value))
 
@@ -20,11 +22,10 @@ class MagicDateOption(Option):
     TYPE_CHECKER["magicdate"] = check_magicdate
 
 
-
 res = [
-        # x time ago
-        (re.compile(
-            r'''^
+    # x time ago
+    (re.compile(
+        r'''^
                 ((?P<weeks>\d+) \s weeks?)?
                 [^\d]*
                 ((?P<days>\d+) \s days?)?
@@ -37,74 +38,74 @@ res = [
                 \s
                 ago
             ''',
-            (re.VERBOSE | re.IGNORECASE)),
-        lambda m: datetime.datetime.today() - datetime.timedelta(
-                days=int(m.group('days') or 0),
-                seconds=int(m.group('seconds') or 0),
-                minutes=int(m.group('minutes') or 0),
-                hours=int(m.group('hours') or 0),
-                weeks=int(m.group('weeks') or 0))),
+        (re.VERBOSE | re.IGNORECASE)),
+     lambda m: datetime.datetime.today() - datetime.timedelta(
+        days=int(m.group('days') or 0),
+        seconds=int(m.group('seconds') or 0),
+        minutes=int(m.group('minutes') or 0),
+        hours=int(m.group('hours') or 0),
+        weeks=int(m.group('weeks') or 0))),
 
-        # Today
-        (re.compile(
-            r'''^
+    # Today
+    (re.compile(
+        r'''^
                 tod                             # Today
             ''',
-            (re.VERBOSE | re.IGNORECASE)),
-        lambda m: datetime.date.today()),
+        (re.VERBOSE | re.IGNORECASE)),
+     lambda m: datetime.date.today()),
 
-        # Now (special case, returns datetime.datetime
-        (re.compile(
-            r'''^
+    # Now (special case, returns datetime.datetime
+    (re.compile(
+        r'''^
                 now                             # Now
             ''',
-            (re.VERBOSE | re.IGNORECASE)),
-        lambda m: datetime.datetime.now()),
-              
-        # Tomorrow
-        (re.compile(
-            r'''^
+        (re.VERBOSE | re.IGNORECASE)),
+     lambda m: datetime.datetime.now()),
+
+    # Tomorrow
+    (re.compile(
+        r'''^
                 tom                             # Tomorrow
             ''',
-            (re.VERBOSE | re.IGNORECASE)),
-        lambda m: datetime.date.today() + datetime.timedelta(days=1)),
-                    
-        # Yesterday
-        (re.compile(
-            r'''^
+        (re.VERBOSE | re.IGNORECASE)),
+     lambda m: datetime.date.today() + datetime.timedelta(days=1)),
+
+    # Yesterday
+    (re.compile(
+        r'''^
                 yes                             # Yesterday
             ''',
-            (re.VERBOSE | re.IGNORECASE)),
-        lambda m: datetime.date.today() - datetime.timedelta(days=1)),
-                    
-        # 4th
-        (re.compile(
-            r'''^
+        (re.VERBOSE | re.IGNORECASE)),
+     lambda m: datetime.date.today() - datetime.timedelta(days=1)),
+
+    # 4th
+    (re.compile(
+        r'''^
                 (?P<day>\d{1,2})                # 4
                 (?:st|nd|rd|th)?                # optional suffix
                 $                               # EOL
             ''',
-            (re.VERBOSE | re.IGNORECASE)),
-        lambda m: datetime.date.today().replace(
-            day=int(m.group('day')))),
-                    
-        # 4th Jan
-        (re.compile(
-            r'''^
+        (re.VERBOSE | re.IGNORECASE)),
+     lambda m: datetime.date.today().replace(
+        day=int(m.group('day')))),
+
+    # 4th Jan
+    (re.compile(
+        r'''^
                 (?P<day>\d{1,2})                # 4
                 (?:st|nd|rd|th)?                # optional suffix
                 \s+                             # whitespace
                 (?P<month>\w+)                  # Jan
                 $                               # EOL
             ''',
-            (re.VERBOSE | re.IGNORECASE)),
-        lambda m: datetime.date.today().replace(
-            day=int(m.group('day')),
-            month=_parseMonth(m.group('month')))),
-                    
-        # 4th Jan 2003
-        (re.compile(
-            r'''^
+        (re.VERBOSE | re.IGNORECASE)),
+     lambda m: datetime.date.today().replace(
+        day=int(m.group('day')),
+        month=_parseMonth(m.group('month')))),
+
+    # 4th Jan 2003
+    (re.compile(
+        r'''^
                 (?P<day>\d{1,2})                # 4
                 (?:st|nd|rd|th)?                # optional suffix
                 \s+                             # whitespace
@@ -114,29 +115,29 @@ res = [
                 (?P<year>\d{4})                 # 2003
                 $                               # EOL
             ''',
-            (re.VERBOSE | re.IGNORECASE)),
-        lambda m: datetime.date(
-            year=int(m.group('year')),
-            month=_parseMonth(m.group('month')),
-            day=int(m.group('day')))),
-                    
-        # Jan 4th
-        (re.compile(
-            r'''^
+        (re.VERBOSE | re.IGNORECASE)),
+     lambda m: datetime.date(
+        year=int(m.group('year')),
+        month=_parseMonth(m.group('month')),
+        day=int(m.group('day')))),
+
+    # Jan 4th
+    (re.compile(
+        r'''^
                 (?P<month>\w+)                  # Jan
                 \s+                             # whitespace
                 (?P<day>\d{1,2})                # 4
                 (?:st|nd|rd|th)?                # optional suffix
                 $                               # EOL
             ''',
-            (re.VERBOSE | re.IGNORECASE)),
-        lambda m: datetime.date.today().replace(
-            day=int(m.group('day')),
-            month=_parseMonth(m.group('month')))),
-                    
-        # Jan 4th 2003
-        (re.compile(
-            r'''^
+        (re.VERBOSE | re.IGNORECASE)),
+     lambda m: datetime.date.today().replace(
+        day=int(m.group('day')),
+        month=_parseMonth(m.group('month')))),
+
+    # Jan 4th 2003
+    (re.compile(
+        r'''^
                 (?P<month>\w+)                  # Jan
                 \s+                             # whitespace
                 (?P<day>\d{1,2})                # 4
@@ -146,15 +147,15 @@ res = [
                 (?P<year>\d{4})                 # 2003
                 $                               # EOL
             ''',
-            (re.VERBOSE | re.IGNORECASE)),
-        lambda m: datetime.date(
-            year=int(m.group('year')),
-            month=_parseMonth(m.group('month')),
-            day=int(m.group('day')))),
+        (re.VERBOSE | re.IGNORECASE)),
+     lambda m: datetime.date(
+        year=int(m.group('year')),
+        month=_parseMonth(m.group('month')),
+        day=int(m.group('day')))),
 
-        # mm/dd/yyyy (American style, default in case of doubt)
-        (re.compile(
-            r'''^
+    # mm/dd/yyyy (American style, default in case of doubt)
+    (re.compile(
+        r'''^
                 (?P<month>0?[1-9]|10|11|12)     # m or mm
                 /                               #
                 (?P<day>0?[1-9]|[12]\d|30|31)   # d or dd
@@ -162,15 +163,15 @@ res = [
                 (?P<year>\d{4})                 # yyyy
                 $                               # EOL
             ''',
-            (re.VERBOSE | re.IGNORECASE)),
-        lambda m: datetime.date(
-            year=int(m.group('year')),
-            month=int(m.group('month')),
-            day=int(m.group('day')))),
+        (re.VERBOSE | re.IGNORECASE)),
+     lambda m: datetime.date(
+        year=int(m.group('year')),
+        month=int(m.group('month')),
+        day=int(m.group('day')))),
 
-        # dd/mm/yyyy (European style)
-        (re.compile(
-            r'''^
+    # dd/mm/yyyy (European style)
+    (re.compile(
+        r'''^
                 (?P<day>0?[1-9]|[12]\d|30|31)   # d or dd
                 /                               #
                 (?P<month>0?[1-9]|10|11|12)     # m or mm
@@ -178,15 +179,15 @@ res = [
                 (?P<year>\d{4})                 # yyyy
                 $                               # EOL
             ''',
-            (re.VERBOSE | re.IGNORECASE)),
-        lambda m: datetime.date(
-            year=int(m.group('year')),
-            month=int(m.group('month')),
-            day=int(m.group('day')))),
+        (re.VERBOSE | re.IGNORECASE)),
+     lambda m: datetime.date(
+        year=int(m.group('year')),
+        month=int(m.group('month')),
+        day=int(m.group('day')))),
 
-        # yyyy-mm-dd (ISO style)
-        (re.compile(
-            r'''^
+    # yyyy-mm-dd (ISO style)
+    (re.compile(
+        r'''^
                 (?P<year>\d{4})                 # yyyy
                 -                               #
                 (?P<month>0?[1-9]|10|11|12)     # m or mm
@@ -194,64 +195,80 @@ res = [
                 (?P<day>0?[1-9]|[12]\d|30|31)   # d or dd
                 $                               # EOL
             ''',
-            (re.VERBOSE | re.IGNORECASE)),
-        lambda m: datetime.date(
-            year=int(m.group('year')),
-            month=int(m.group('month')),
-            day=int(m.group('day')))),
+        (re.VERBOSE | re.IGNORECASE)),
+     lambda m: datetime.date(
+        year=int(m.group('year')),
+        month=int(m.group('month')),
+        day=int(m.group('day')))),
 
-        # yyyymmdd
-        (re.compile(
-            r'''^
+    # yyyymmdd
+    (re.compile(
+        r'''^
                 (?P<year>\d{4})                 # yyyy
                 (?P<month>0?[1-9]|10|11|12)     # m or mm
                 (?P<day>0?[1-9]|[12]\d|30|31)   # d or dd
                 $                               # EOL
             ''',
-            (re.VERBOSE | re.IGNORECASE)),
-        lambda m: datetime.date(
-            year=int(m.group('year')),
-            month=int(m.group('month')),
-            day=int(m.group('day')))),
+        (re.VERBOSE | re.IGNORECASE)),
+     lambda m: datetime.date(
+        year=int(m.group('year')),
+        month=int(m.group('month')),
+        day=int(m.group('day')))),
 
-        # next Tuesday
-        (re.compile(
-            r'''^
+    # next Tuesday
+    (re.compile(
+        r'''^
                 next                            # next
                 \s+                             # whitespace
                 (?P<weekday>\w+)                # Tuesday
                 $                               # EOL
             ''',
-            (re.VERBOSE | re.IGNORECASE)),
-        lambda m: _nextWeekday(_parseWeekday(m.group('weekday')))),
+        (re.VERBOSE | re.IGNORECASE)),
+     lambda m: _nextWeekday(_parseWeekday(m.group('weekday')))),
 
-        # last Tuesday
-        (re.compile(
-            r'''^
+    # last Tuesday
+    (re.compile(
+        r'''^
                 (last                           # last
                 \s+)?                           # whitespace
                 (?P<weekday>\w+)                # Tuesday
                 $                               # EOL
             ''',
-            (re.VERBOSE | re.IGNORECASE)),
-        lambda m: _lastWeekday(_parseWeekday(m.group('weekday')))),
-       ]
+        (re.VERBOSE | re.IGNORECASE)),
+     lambda m: _lastWeekday(_parseWeekday(m.group('weekday')))),
+]
 
 
 def _parseMonth(input):
-    months = "January February March April May June July August September October November December".split(' ')
+    months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ]
     for i, month in enumerate(months):
         p = re.compile(input, re.IGNORECASE)
-        if p.match(month): return i+1
+        if p.match(month):
+            return i + 1
     else:
         raise Exception
 
 
 def _parseWeekday(input):
-    days = "Monday Tuesday Wednesday Thursday Friday Saturday Sunday".split(' ')
+    days = "Monday Tuesday Wednesday Thursday Friday Saturday Sunday".split(
+        ' ')
     for i, day in enumerate(days):
         p = re.compile(input, re.IGNORECASE)
-        if p.match(day): return i
+        if p.match(day):
+            return i
     else:
         raise Exception
 
@@ -273,9 +290,25 @@ def _lastWeekday(weekday):
 
 
 def magicdate(input, tz=None):
-    if tz is not None: os.environ['TZ'] = tz
+    if tz is not None:
+        os.environ['TZ'] = tz
 
     for r, f in res:
         m = r.match(input.strip())
         if m:
             return f(m)
+
+
+def main():
+    import sys
+
+    parser = OptionParser()
+    parser.add_option('-f', '--format', dest='fmt', default='%Y/%m/%d')
+    options, args = parser.parse_args()
+    output = ' '.join(magicdate(arg).strftime(options.fmt) for arg in args)
+    sys.stdout.write(output)
+    sys.stdout.write('\n')
+
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
+from setuptools import setup
 RELEASE = True
-
-from setuptools import setup, find_packages
-import sys, os
 
 classifiers = """\
 Development Status :: 4 - Beta
@@ -13,42 +11,47 @@ Programming Language :: Python
 Topic :: Software Development :: Libraries :: Python Modules
 """
 
-version = '0.1.3'
+version = '0.2'
 
-setup(name='magicdate',
-      version=version,
-      description="Convert fuzzy date to a datetime object.",
-      long_description="""\
-Convert from fuzzy dates like "yesterday", "2 weeks and 1 day ago", "next wed", "Jan 4", etc., to a datetime object.
+setup(
+    name='magicdate',
+    version=version,
+    description="Convert fuzzy date to a datetime object.",
+    long_description="""
+Convert from fuzzy dates like "yesterday", "2 weeks and 1 day ago", "next wed",
+"Jan 4", etc., to a datetime object.
 
 This is useful for processing command line arguments::
 
     >>> from optparse import OptionParser
     >>> import magicdate
     >>> parser = OptionParser(option_class=magicdate.MagicDateOption)
-    >>> parser.add_option('-s', '--start', dest='start', type='magicdate', default=None)
-    >>> parser.add_option('-e', '--end', dest='end', type='magicdate', default='today')
-    
+    >>> parser.add_option(
+    ...     '-s', '--start', dest='start', type='magicdate', default=None)
+    >>> parser.add_option(
+    ...     '-e', '--end', dest='end', type='magicdate', default='today')
+
 Now you can pass options like "today", "1996-01-01", etc., to your program.
 
 Inspired by Simon Willison's ``dateparse.js``.
 
 """,
-      classifiers=filter(None, classifiers.split("\n")),
-      keywords='datetime time',
-      author='Roberto De Almeida',
-      author_email='rob@pydap.org',
-      #url='http://dealmeida.net/projects/magicdate',
-      download_url = "http://cheeseshop.python.org/packages/source/m/magicdate/magicdate-%s.tar.gz" % version,
-      license='MIT',
-      py_modules=['magicdate'],
-      include_package_data=True,
-      zip_safe=True,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
-      
+    classifiers=filter(None, classifiers.split("\n")),
+    keywords='datetime time',
+    author='Beto Dealmeida',
+    author_email='roberto@dealmeida.net',
+    url='https://github.com/betodealmeida/magicdate',
+    download_url=(
+        "http://cheeseshop.python.org/packages/source/m/magicdate/"
+        "magicdate-%s.tar.gz" % version
+    ),
+    license='MIT',
+    py_modules=['magicdate'],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[],
+    entry_points="""
+        [console_scripts]
+        magicdate = magicdate:main
+    """,
+)
